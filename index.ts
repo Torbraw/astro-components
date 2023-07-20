748e11e3acd8a70829e0d86e4a32793e81edbe39
Maxime <gendron.maxime1@gmail.com>
2023-07-20 19:41:35 -0400

    feat: Added missing card components

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ export { default as Card } from './src/card/card.astro';
 export { default as CardHeader } from './src/card/card-header.astro';
 export { default as CardTitle } from './src/card/card-title.astro';
 export { default as CardDescription } from './src/card/card-header.astro';
+export { default as CardContent } from './src/card/card-content.astro';
+export { default as CardFooter } from './src/card/card-footer.astro';
