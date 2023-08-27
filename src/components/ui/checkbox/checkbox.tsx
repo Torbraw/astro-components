aa481cc50403a5ef3b9cece91e4f7662ba87ac6c
Maxime <gendron.maxime1@gmail.com>
2023-08-26 20:06:49 -0400

    fix: Added type to checkbox

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -13,6 +13,7 @@ export const Checkbox: Component<Props> = (props) => {
     <button
       data-checked={local.checked}
       onClick={() => local.onCheckedChange(!local.checked)}
+      type="button"
       class={twMerge(
         'peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[checked=true]:bg-primary data-[checked=true]:text-primary-foreground',
         local.class,
