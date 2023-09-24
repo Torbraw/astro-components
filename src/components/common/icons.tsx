20c79e5f20b5e9dcefb8f3af9df4de4e20ea811f
Maxime <gendron.maxime1@gmail.com>
2023-09-24 18:50:46 -0400

    fix: fillColor menuicon

--- src/components/common/icons.tsx
+++ src/components/common/icons.tsx
@@ -300,7 +300,12 @@ export const MenuIcon: ParentComponent<ComponentProps<'svg'>> = (props) => {
 
   return (
     <svg class={local.class} {...rest} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-      <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z" />
+      <path
+        d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"
+        fill="currentColor"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+      />
     </svg>
   );
 };
