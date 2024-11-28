ecd708f19952cf72beaca51f838588c1a05d609e
Maxime <gendron.maxime1@gmail.com>
2024-11-27 23:49:08 -0500

    chore: update select icon

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -27,8 +27,19 @@ const SelectTrigger = <T extends ValidComponent = "button">(props: PolymorphicPr
       {...others}
     >
       {local.children}
-      <SelectPrimitive.Icon>
-        <ArrowDownIcon class="h-4 w-4 opacity-50" />
+      <SelectPrimitive.Icon
+        as="svg"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="size-4 opacity-50"
+      >
+        <path d="M8 9l4 -4l4 4" />
+        <path d="M16 15l-4 4l-4 -4" />
       </SelectPrimitive.Icon>
     </SelectPrimitive.Trigger>
   );
