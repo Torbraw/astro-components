9ca405eb0c47267d8492fb37c37033ae6f2c53ca
Maxime <gendron.maxime1@gmail.com>
2024-12-01 11:59:10 -0500

    chore: format

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,17 +1,8 @@
-import { type Component, type ComponentProps, splitProps } from 'solid-js';
-import { cn } from '@/lib/utils';
+import { type Component, type ComponentProps, splitProps } from "solid-js";
+import { cn } from "../../lib/utils";
 
-export const Skeleton: Component<ComponentProps<'div'>> = (props) => {
-  const [local, rest] = splitProps(props, ['class']);
+export const Skeleton: Component<ComponentProps<"div">> = (props) => {
+  const [local, rest] = splitProps(props, ["class"]);
 
-  return (
-    
-    <div
-      class={cn(
-        'animate-pulse rounded-md bg-muted',
-        local.class,
-      )}
-      {...rest}
-    />
-  );
+  return <div class={cn("animate-pulse rounded-md bg-muted", local.class)} {...rest} />;
 };
